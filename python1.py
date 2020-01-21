fd96976d0bd17a9065b6f5721a87101544488257
ryasun85 <59563060+ryasun85@users.noreply.github.com>
2020-01-21 14:40:23 +0000

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,11 @@
+time = 500
+
+place_of_work = "CodeNation"
+town_of_home = "Manchester"
+
+if time < 700: print("sleeping")
+elif time > 700 and time < 830 : 
+    print("I am at {}".format(place_of_work))
+else:
+    print("I am in{}".format(town_of_home))
+    
